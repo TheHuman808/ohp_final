a957e667be79db1aa4fbb4d4513ae08b180dc7d9
TheHuman808 <your-email@example.com>
2025-10-19 00:19:36 +0300

    Fix asset paths for telegram-bot

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/reffer/',
+  base: '/telegram-bot/',
   server: {
     host: "::",
     port: 8080,
