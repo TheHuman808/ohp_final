31cd0c8b0c73629df73e7ae898fcdabe8ca502fd
TheHuman808 <alex.brasaus@gmail.com>
2025-12-15 13:09:54 +0300

    Обновлен URL Apps Script на новый ID развертывания

--- src/components/TestAppsScriptConnection.tsx
+++ src/components/TestAppsScriptConnection.tsx
@@ -16,7 +16,7 @@ const TestAppsScriptConnection = () => {
   const envApiKey = import.meta.env.VITE_GOOGLE_SHEETS_API_KEY;
 
   // Fallback значения из googleSheetsService
-  const fallbackWebAppUrl = 'https://script.google.com/macros/s/AKfycbwMXMlXIEBBZ7S5mAexa2aSfRdbXHnbsZ4gyzC0TOwHGiIB5FGLNVg7v5zS4A4CY2Dh/exec';
+  const fallbackWebAppUrl = 'https://script.google.com/macros/s/AKfycbyD5DH7bPGln18qfBSUuJxgIMxM3Kz6Aap_TdowIAj9D-hNsnq1H-2CKG_gzF-PjwJ0/exec';
   const fallbackSheetsId = '1fh4-V4n0ho-RF06xcxl0JYxK5xQf8WOMSYy-tF6vRkU';
   const fallbackApiKey = 'AIzaSyD1-O9ID7-2EFVum1ITNRyrhJYtvlY5wKg';
 
