0973a36a0b1b774918e4ba86fb20336cd35a006f
TheHuman808 <alex.brasaus@gmail.com>
2025-12-17 16:35:30 +0300

    Обновлен Apps Script URL на AKfycbyBgbIVz3FU3kQ8dHS_yxaEnJrz-4dIh1bhpZGUk44AFe-EzFbZ-kdXTBuh2D6Uh0Lu

--- src/components/TestAppsScriptConnection.tsx
+++ src/components/TestAppsScriptConnection.tsx
@@ -16,7 +16,7 @@ const TestAppsScriptConnection = () => {
   const envApiKey = import.meta.env.VITE_GOOGLE_SHEETS_API_KEY;
 
   // Fallback значения из googleSheetsService
-  const fallbackWebAppUrl = 'https://script.google.com/macros/s/AKfycby2IHyHPiG5r-hZ9ga2IsP5F4LELsAl2kOot4gYZ0qA4e3gnbPdHx-zsKn8O_oL8W75/exec';
+  const fallbackWebAppUrl = 'https://script.google.com/macros/s/AKfycbyBgbIVz3FU3kQ8dHS_yxaEnJrz-4dIh1bhpZGUk44AFe-EzFbZ-kdXTBuh2D6Uh0Lu/exec';
   const fallbackSheetsId = '1fh4-V4n0ho-RF06xcxl0JYxK5xQf8WOMSYy-tF6vRkU';
   const fallbackApiKey = 'AIzaSyD1-O9ID7-2EFVum1ITNRyrhJYtvlY5wKg';
 
