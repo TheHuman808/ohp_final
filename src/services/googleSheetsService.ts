6c49b9dcf96fa8aad132276b66d18fceb652359c
TheHuman808 <alex.brasaus@gmail.com>
2025-12-17 16:01:09 +0300

    Обновлен Apps Script URL на AKfycbwUQHchYlQz0PQi3y3lbTKJS8eRhestaUkZP3eM7doFuVX7dd062_e3HjDxyjBc9kop

--- src/services/googleSheetsService.ts
+++ src/services/googleSheetsService.ts
@@ -62,7 +62,7 @@ class GoogleSheetsService {
   constructor() {
     this.spreadsheetId = import.meta.env.VITE_GOOGLE_SHEETS_ID || '1fh4-V4n0ho-RF06xcxl0JYxK5xQf8WOMSYy-tF6vRkU';
     this.apiKey = import.meta.env.VITE_GOOGLE_SHEETS_API_KEY || 'AIzaSyD1-O9ID7-2EFVum1ITNRyrhJYtvlY5wKg';
-    this.webAppUrl = import.meta.env.VITE_GOOGLE_APPS_SCRIPT_URL || 'https://script.google.com/macros/s/AKfycby7uoRsLAJNKgeK2qqLWFVZ5lX-1vtLKE_bFkWSUmHM0hBDD8xbMrSlHzaTdyObfNyv/exec';
+    this.webAppUrl = import.meta.env.VITE_GOOGLE_APPS_SCRIPT_URL || 'https://script.google.com/macros/s/AKfycbwUQHchYlQz0PQi3y3lbTKJS8eRhestaUkZP3eM7doFuVX7dd062_e3HjDxyjBc9kop/exec';
     
     if (!this.apiKey || !this.spreadsheetId) {
       console.warn('Google Sheets API не настроен полностью. Установите переменные окружения VITE_GOOGLE_SHEETS_API_KEY и VITE_GOOGLE_SHEETS_ID');
